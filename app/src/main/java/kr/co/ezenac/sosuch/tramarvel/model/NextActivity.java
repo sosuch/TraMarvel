84a45d48bc55351318b58707eed2c4495c99098b
SongSungchul <sosuch82@hotmail.com>
2018-02-20 10:44:40 +0900

    GIF 파일 구현 (GIF 파일 작은용량으로 임시 변경)

--- app/src/main/java/kr/co/ezenac/sosuch/tramarvel/model/NextActivity.java
+++ app/src/main/java/kr/co/ezenac/sosuch/tramarvel/model/NextActivity.java
@@ -1,20 +1,16 @@
 package kr.co.ezenac.sosuch.tramarvel.model;
 
 import android.content.Intent;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
-import kr.co.ezenac.sosuch.tramarvel.JapanActivity2;
 import kr.co.ezenac.sosuch.tramarvel.MainActivity;
-import kr.co.ezenac.sosuch.tramarvel.OptionActivity;
 import kr.co.ezenac.sosuch.tramarvel.R;
 
 public class NextActivity extends AppCompatActivity {
@@ -26,12 +22,12 @@ public class NextActivity extends AppCompatActivity {
         ImageView image_gif = (ImageView)findViewById(R.id.image_gif);
         RelativeLayout move_next = (RelativeLayout)findViewById(R.id.move_next);
         GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(image_gif);
-        Glide.with(this).load(R.raw.anigif).into(imageViewTarget);
+        Glide.with(this).load(R.raw.gifexample).into(imageViewTarget);
 
         move_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(NextActivity.this,JapanActivity2.class);
+                Intent intent = new Intent(NextActivity.this,MainActivity.class);
                 startActivityForResult(intent,0);
             }
         });
